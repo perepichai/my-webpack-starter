6977251024527d23407efa1900a82672eb951d27
perepichai <o.perepichai@gmail.com>
2021-10-21 22:34:37 +0300

    feat: add image support

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,25 @@ if (process.env.NODE_ENV === "production") {
 }
 module.exports = {
     mode: mode,
+    target: target,
     plugins: [new MiniCssExtractPlugin()],
+    output: {
+        assetModuleFilename: "assets/images/[hash][ext][query]",
+    },
     module: {
         rules: [
             {
                 test: /\.(s[ac]|c)ss$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {publicPath: ""}
+                    },
+                    "css-loader", "postcss-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset/resource",
             },
             {
                 test: /\.js$/,
